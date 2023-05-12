56ab0224c23b307bfef3ba3be72e84e9b9910c91
anthony-kostalvazquez <anthonykostal@gmail.com>
2023-05-12 10:27:58 -0600

    Testing my commit skills

--- C_Programs/Learning_C/Reduction_Of_BQF.c
+++ C_Programs/Learning_C/Reduction_Of_BQF.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-main()
+int main()
 {
-
     //initialization
     int Input_Poly[3];
     char text_array[] = {'a', 'b', 'c'};
 
     //input loop
     printf("Please Input Your Quadratic Form ax^2 + bxy + cy^2 one variable at a time\n");
-
     for(int i = 0; i < 3; i++)
     {
         printf("Please input your %c value \n", text_array[i]);
-        scanf("%d");
+        scanf("%d", &Input_Poly[i]);
     }
-    printf("Your input is %dx^2 + %dxy + %dy^2", Input_Poly[0], Input_Poly[1], Input_Poly[2]);
+    printf("Your input is %dx^2 + %dxy + %dy^2\n", Input_Poly[0], Input_Poly[1], Input_Poly[2]);
+
+    //Replacement loop
     
 
 
