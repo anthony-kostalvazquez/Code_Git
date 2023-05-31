3fbf9467c851b35cea9dcdaf3b337492fbbdce0d
anthony-kostalvazquez <anthonykostal@gmail.com>
2023-05-31 16:50:32 -0600

    semi working might explode tho

--- C_Programs/CPSC331_DataStructures_Algorithms/Assignment4.c
+++ C_Programs/CPSC331_DataStructures_Algorithms/Assignment4.c
@@ -10,88 +10,63 @@
 
 //#define NDEBUG
 
+
+
 int main()
 {
-    //initalizes the variables i need to time the various searches
+    //initalizes the variables to time the various searches
     clock_t start, end;
     double cpu_time_used;
 
+
     //You will generate 99 distinct random numbers between 0 and 5000 and store them in an array of size 100. Add 5001 as the 100th cell in the array.
     int E_size = 100;
-    int *ElementsArray;
-    ElementsArray = InitRandIntArray(E_size, 5000, true);
+    int *ElementsArray = InitRandIntArray(E_size, 5000, true);
     ElementsArray[99] = 5001;
 
-    //generate an array of integers called the search array. This is the array to be searched. The array size n is between 1000 and 1,000,000 and the values in the array are from 0 to 5000. 
-    int S_size = (rand() % (1000000 - 1000) + 1000);
-    int *SearchArray;
-    SearchArray = InitRandIntArray(S_size, 5000, false);
-
-    //SEARCHES
-    for (int S_size = 1000; i < count; i++)
+    //creates random search arrays of size 1,000 - 1,000,000 
+    for (int S_size = 5000; S_size <= 5005; S_size++)
     {
-        /* code */
-    }
-    
-
-    //----------SEQUENTIAL SEARCH----------//
-    start = clock();
-    //searches the search array for the elements in the elements array
-    for (int i = 0; ElementsArray[i] != '\0'; i++)
-    {
-        int tmp = SequentialSearch(ElementsArray[i], SearchArray);
         
-        assert(SearchArray[tmp] == ElementsArray[i] || tmp == -1);
+        //generate an array of integers called the search array. This is the array to be searched. the values in the array are from 0 to 5000.
+        int *SearchArray = InitRandIntArray(S_size, 5000, false);
+
+        //resets the times to 0
+        double SeqTime = 0;
+        double BinTime = 0;
+        double HashTime = 0;
+
+        //searches the search array for the elements in the elements array
+        for (int i = 0; ElementsArray[i] != '\0'; i++)
+        {
+            //--------------------SEQUENTIAL SEARCH--------------------//
+            start = clock();
+            int tmp1 = SequentialSearch(ElementsArray[i], SearchArray);
+            end = clock();
+
+            SeqTime += ((double) (end - start)) / CLOCKS_PER_SEC;
+            assert(SearchArray[tmp1] == ElementsArray[i] || tmp1 == -1);
+            //--------------------HASH SEARCH--------------------//
+
+
+
+            //--------------------BINARY SEARCH--------------------//
+            BubbleSort(SearchArray);
+            start = clock();
+            int tmp2 = BinarySearch(ElementsArray[i], SearchArray, 0, S_size);
+            end = clock();
+            
+            BinTime += ((double) (end - start)) / CLOCKS_PER_SEC;
+            assert(SearchArray[tmp2] == ElementsArray[i] || tmp2 == -1);
+        }
+    
+        printf("for array of size %d Sequential search took %fs\n", S_size, SeqTime);
+        printf("for array of size %d Binary search took %fs\n", S_size, BinTime);
+    
+        //frees the memory of the search array
+        free(SearchArray);
     }
-    end = clock();
 
-    //printing off the time taken
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("The sequential search took %f seconds to execute\n", cpu_time_used);
-
-
-    //----------BINARY SEARCH----------//
-    BubbleSort(SearchArray);
-
-    start = clock();
-    //searches the search array for the elements in the elements array
-    for (int i = 0; ElementsArray[i] != '\0'; i++)
-    {
-        int tmp = BinarySearch(ElementsArray[i], SearchArray, 0, S_size);
-
-        assert(SearchArray[tmp] == ElementsArray[i] || tmp == -1);
-    }
-    end = clock();
-
-    //printing off the time taken
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("The sequential search took %f seconds to execute\n", cpu_time_used);
-
-    /*
-    for (int i = 0; i < 10000; i++)
-    {
-        int randnum = rand() % 5000;
-        printf("the index of %d is %d\n", i, SequentialSearch(i, ElementsArray));
-    }
-
-    for (int i = 0; ElementsArray[i] != '\0'; i++)
-    {
-        int tmp = SequentialSearch(ElementsArray[i], SearchArray);
-        printf("the index of %d is %d\nPROOF E_ARR[%d] = %d & S_ARR[%d] = %d\n",ElementsArray[i], tmp, i, ElementsArray[i], tmp, SearchArray[tmp]);
-    }
-
-    PrintIntArray(ElementsArray, 2);
-
-    */
-
-   
-
-
-
-
-
-
-
-
+    
     return 0;
 }
