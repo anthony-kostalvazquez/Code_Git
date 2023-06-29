aba985f8797c4a39bebb431fa2486305d9b78ef2
anthony-kostalvazquez <anthonykostal@gmail.com>
2023-06-29 16:23:38 -0600

    Need to wait until i have linux then i can start dealing with this

---
+++ Cpp/FreeCodeCampCorse/3.FirstSteps/First_Program.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+consteval int get_value()
+{
+    return 3;
+}
+
+int main()
+{
+    constexpr int value = get_value();
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
