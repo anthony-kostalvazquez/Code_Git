4c9a79c384a6e0442784c8dee000d984c605807a
anthony kostal-vazquez <anthonykostal@gmail.com>
2023-07-25 15:47:10 -0600

    finshed chapter 8 of learn cpp (Finally getting into some good stuff)

---
+++ Cpp/LearnCPP.com Tutorials/Chapter_8(Type Conversion and Function Overloading)/Quiz5c.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// write your sub function template here
+auto sub(auto x, auto y)
+{
+    return x - y;
+    
+}
+
+
+
+
+
+int main()
+{
+	std::cout << sub(3, 2) << '\n';
+	std::cout << sub(3.5, 2) << '\n';
+	std::cout << sub(4, 1.5) << '\n';
+
+	return 0;
+}
